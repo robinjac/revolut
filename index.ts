8b400e29c4ec7534423d37b9ec79e38f97cddfd0
robin jacobsson <robin23jac@gmail.com>
2024-07-08 09:16:02 +0200

    Get ticks

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import tickers from "./tickers.json";
+import headers from "./headers.json";
+
+const url = "https://trade.revolut.com/api/retail/instruments/tickers";
+
+const res = await fetch(url, {
+  method: "POST",
+  headers,
+  body: JSON.stringify(tickers),
+});
+
+const result = await res.json();
+
+console.log(result.length, result[result.length - 1]);
